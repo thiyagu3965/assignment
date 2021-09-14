5106776497cc9784c5dea8121e4884c9361d97eb
thiyagu3965 <softsolsalem@gmail.com>
2021-09-14 13:05:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,165 @@
+
+import './App.css';
+import logonav from "./purplelock.png"
+function App() {
+  return (
+    <div className="container">
+    <div className="navbar">
+      <ul className="list">
+        <li >
+          <img className="navlogo" src={logonav} />
+          <p>Lock Ness</p>
+        </li>
+        <li className="lii">     
+          <span  className="fa fa-user fa-2x power"></span>
+          <span   className="fa fa-power-off fa-2x power"></span>
+        </li>
+      
+      </ul>
+    </div>
+    <div className="secondRow">
+      <div className="twobutton">
+        <ul >
+          <li><button className="btn">Create Escrow</button></li>
+          <li><button className="btn">Connect Wallet</button></li>
+        </ul>
+      </div>
+      </div>
+      <div className="fortbl">
+<table>
+  <thead className="headingRow">
+  <tr >
+    <th>Escrow No</th>
+    <th>Name</th>
+    <th>Amount</th>
+    <th>Method</th>
+    <th>Type</th>
+    <th>To/Form</th>
+    <th>Created On</th>
+    <th>Completed On</th>
+    <th>Status</th>
+    <th>Action</th>
+  </tr>
+  </thead>
+  <tr>
+    <td>Esc100Xc1</td>
+    <td>iphone 6s</td>
+    <td>450 BnB</td>
+    <td>Single</td>
+    <td>Sent</td>
+    <td>3x13*****45</td>
+    <td>27-Aug-21</td>
+    <td>1-Sep-21</td>
+    <td>Active</td>
+    <td><span className="fa fa-eye"></span></td>
+    
+  </tr>
+  <tr>
+    <td>Esc100Xc1</td>
+    <td>iphone 6s</td>
+    <td>450 BnB</td>
+    <td>Single</td>
+    <td>Sent</td>
+    <td>3x13*****45</td>
+    <td>27-Aug-21</td>
+    <td>1-Sep-21</td>
+    <td>Active</td>
+    <td><span className="fa fa-eye"></span></td>
+    
+  </tr>
+  <tr>
+    <td>Esc100Xc1</td>
+    <td>iphone 6s</td>
+    <td>450 BnB</td>
+    <td>Single</td>
+    <td>Sent</td>
+    <td>3x13*****45</td>
+    <td>27-Aug-21</td>
+    <td>1-Sep-21</td>
+    <td>Active</td>
+    <td><span className="fa fa-eye"></span></td>
+    
+  </tr>
+  <tr>
+    <td>Esc100Xc1</td>
+    <td>iphone 6s</td>
+    <td>450 BnB</td>
+    <td>Single</td>
+    <td>Sent</td>
+    <td>3x13*****45</td>
+    <td>27-Aug-21</td>
+    <td>1-Sep-21</td>
+    <td>Active</td>
+    <td><span className="fa fa-eye"></span></td>
+    
+  </tr>
+  <tr>
+    <td>Esc100Xc1</td>
+    <td>iphone 6s</td>
+    <td>450 BnB</td>
+    <td>Single</td>
+    <td>Sent</td>
+    <td>3x13*****45</td>
+    <td>27-Aug-21</td>
+    <td>1-Sep-21</td>
+    <td>Active</td>
+    <td><span className="fa fa-eye"></span></td>
+    
+  </tr>
+  <tr>
+    <td>Esc100Xc1</td>
+    <td>iphone 6s</td>
+    <td>450 BnB</td>
+    <td>Single</td>
+    <td>Sent</td>
+    <td>3x13*****45</td>
+    <td>27-Aug-21</td>
+    <td>1-Sep-21</td>
+    <td>Active</td>
+    <td><span className="fa fa-eye"></span></td>
+    
+  </tr>
+  <tr>
+    <td>Esc100Xc1</td>
+    <td>iphone 6s</td>
+    <td>450 BnB</td>
+    <td>Single</td>
+    <td>Sent</td>
+    <td>3x13*****45</td>
+    <td>27-Aug-21</td>
+    <td>1-Sep-21</td>
+    <td>Active</td>
+    <td><span className="fa fa-eye"></span></td>
+    
+  </tr>
+  <tr>
+    <td>Esc100Xc1</td>
+    <td>iphone 6s</td>
+    <td>450 BnB</td>
+    <td>Single</td>
+    <td>Sent</td>
+    <td>3x13*****45</td>
+    <td>27-Aug-21</td>
+    <td>1-Sep-21</td>
+    <td>Active</td>
+    <td><span className="fa fa-eye"></span></td>
+    
+  </tr>
+</table>
+</div>
+
+    <div className="footer">
+    <ul >
+          <li>Help</li>
+          <li><span className="vl"></span></li>
+
+          <li>Privacy Policy</li>
+          <li><span className="vl"></span></li>
+          <li>Contact Us</li>
+        </ul>
+    </div>
+    </div> 
+  );
+}
+
+export default App;
